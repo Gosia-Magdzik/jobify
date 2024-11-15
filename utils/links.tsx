53a5cf92b224733744a7cc65900ec31e82666fc8
Gosia Magdzik <melassa26@gmail.com>
2024-11-15 19:59:35 +0100

    links utils

--- utils/links.tsx
+++ utils/links.tsx
@@ -5,3 +5,23 @@ type NavLink = {
     label: string;
     icon: React.ReactNode;
   };
+
+  const links: NavLink[] = [
+    {
+      href: '/add-job',
+      label: 'add job',
+      icon: <Layers />,
+    },
+    {
+      href: '/jobs',
+      label: 'all jobs',
+      icon: <AppWindow />,
+    },
+    {
+      href: '/stats',
+      label: 'stats',
+      icon: <AreaChart />,
+    },
+  ];
+
+  export default links;
