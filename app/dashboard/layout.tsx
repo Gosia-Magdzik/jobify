da5cb6718d8d3ce730cf9c49e941e29d76fa9a59
Gosia Magdzik <melassa26@gmail.com>
2024-11-15 21:07:16 +0100

    layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Navbar from '@/components/ui/Navbar';
+import Sidebar from '@/components/ui/Sidebar';
 
-function layout({children}:{children:React.ReactNode}) {
+import { PropsWithChildren } from 'react';
+
+function layout({children}:PropsWithChildren) {
   return (
-    <div>{children}</div>
+    <main className='grid lg:grid-cols-5'>
+      <div className='hidden lg:block lg:col-span-1 lg:min-h-screen'>
+        <Sidebar/>
+      </div>
+      <div className='lg:col-span-4'>
+        <Navbar/>
+      </div>
+      <div className='py-16 px-4 sm:px-8 lg:px-16'>
+        {children}
+      </div>
+      {children}
+    </main>
   )
 }
 
