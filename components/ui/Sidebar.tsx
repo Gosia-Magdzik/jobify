b4b5ba2571c33aa6438efb88f0bfe79c467e6cde
Gosia Magdzik <melassa26@gmail.com>
2024-11-20 18:04:05 +0100

    Import necessary modules and components

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,4 +1,10 @@
-
+'use client';
+import Logo from '@/assets/images/logo.svg';
+import links from '@/utils/links';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
 
 function Sidebar() {
 
