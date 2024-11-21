b32aae0e2b511e225c4d379d60380faa36f21b6b
Gosia Magdzik <melassa26@gmail.com>
2024-11-21 20:10:44 +0100

    styling has been added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -5,11 +5,11 @@ import ThemeToggle from "./ThemeToggle";
 
 function Navbar() {
   return (
-    <nav className=''>
+    <nav className='bg-muted py-4 flex items-center justify-between py-4 sm:px-16 lg:px-24'>
       <div>
         <LinksDropdown/>
       </div>
-      <div>
+      <div className="flex items-center gap-x-4">
         <ThemeToggle/>
         <UserButton/>
       </div>
